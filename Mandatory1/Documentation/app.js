f35f4a52c25495020c21cfc5b3c5c735478dc331
MarkusDrejer <44167365+MarkusDrejer@users.noreply.github.com>
2020-03-06 23:38:37 +0100

    Add files via upload

---
+++ Mandatory1/Documentation/app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const fs = require('fs');
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+    return res.redirect("/theory")
+});
+
+app.get("/commands", (req, res) => {
+    return res.sendFile(__dirname + "/public/html/commands.html");
+});
+
+app.get("/tools", (req, res) => {
+    return res.sendFile(__dirname + "/public/html/tools.html");
+});
+
+app.get("/theory", (req, res) => {
+    return res.sendFile(__dirname + "/public/html/theory.html");
+});
+
+app.get("/imgFiles", (req, res) => {
+    const imgFolder = __dirname + '/public/img';
+    const imgFiles = [];
+    fs.readdirSync(imgFolder).forEach(file => {
+        imgFiles.push(file);
+      });
+      return res.send(imgFiles);
+});
+
+const server = app.listen(3000, (error) => {
+    if (error) {
+        console.log("Error ocurred while trying to run server");
+    }
+    console.log("Server running on port:", server.address().port);
+});
---
+++ Mandatory1/Documentation/public/script/index.js
@@ -0,0 +1,51 @@
+let imgFiles = [];
+    $.get('/imgFiles', (data) => {
+        imgFiles = data;
+    });
+
+$(document).ready(() => {
+
+    $(".openbtn").click(() => {
+        $("#mySidebar").toggleClass("open");
+        $("#main").toggleClass("move");
+    });
+
+    $(".closebtn").click(() => {
+        $("#mySidebar").css("width", "0");
+        $("#main").css("margin-left", "0");
+        $(event.currentTarget).attr("class", "openbtn");
+    });
+
+    $(".openbtn").mouseenter(() => {
+        $(event.currentTarget).text("Side-Menu");
+    }).mouseout(() => {
+        $(event.currentTarget).text("☰");
+    });
+
+    $(".examples").mouseenter(() => {
+        let currentTagId = $(event.currentTarget).attr("id");
+        let count = 1;
+
+        for(let i = 0; i < imgFiles.length; i += 1) {
+            const firstDigit = imgFiles[i].search(/\d/);
+            const trackedFileId = imgFiles[i].substr(0, firstDigit);
+            const trackedNumber = imgFiles[i].charAt(firstDigit);
+
+            if(trackedFileId === currentTagId) {
+                $("#ex" + trackedNumber).css("visibility", "visible");
+                $("#ex" + trackedNumber).attr("src", "img/" + imgFiles[i]);
+                count += 1;
+            }
+        }
+        for(count; count <= 3; count += 1) {
+            $("#ex" + count).css("visibility", "hidden");
+            console.log(count);
+        }
+    });
+
+    $("#npmC").click(() => {
+        $("#ex1").attr("src", "img/npmC4.png");
+        $("#ex2").attr("src", "img/npmC5.png");
+        $("#ex3").attr("src", "img/npmC6.png");
+    });
+});
